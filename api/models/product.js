c462f51661e77c20ba8292543200008d62025427
zaharia93 <84892237+zaharia93@users.noreply.github.com>
2021-06-28 08:40:16 +0300

    Add files via upload

---
+++ api/models/product.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    ssubcategorie: { type: mongoose.Schema.Types.ObjectId, ref: 'Ssubcategorie', required: true },
+    //  productImage: { type: String, required: true },
+    
+     
+});
+
+module.exports = mongoose.model('Product', productSchema); 
+
